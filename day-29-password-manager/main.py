46571d1e98fee33f8f46d974386c8549579e8c32
rifqiay <mrifqiay@gmail.com>
2024-11-11 14:16:36 +0700

    add day-29 final

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -1,5 +1,32 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters) ]
+    password_letters += [random.choice(symbols) for _ in range(nr_symbols)]
+    password_letters += [random.choice(numbers) for _ in range(nr_numbers)]
+    random.shuffle(password_letters)
+    password = "".join(password_letters)
+
+    if len(password_entry.get()) > 0:
+        password_entry.delete(0, END)
+        password_entry.insert(0, password)
+        pyperclip.copy(password)
+    else:
+        password_entry.insert(0, password)
+        pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -8,13 +35,17 @@ def save():
     username_email = username_email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", mode="a") as data:
-        data.write(f"{website} | {username_email} | {password}\n")
-    
-    website_entry.delete(0, END)
-    username_email_entry.delete(0, END)
-    password_entry.delete(0, END)
-    website_entry.focus()
+    if len(website) == 0 or len(username_email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Opps", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"The account details is:\n Username/Email: {username_email}\n Password: {password}")
+        if is_ok:
+            with open("data.txt", mode="a") as data:
+                data.write(f"{website} | {username_email} | {password}\n")
+                website_entry.delete(0, END)
+                username_email_entry.delete(0, END)
+                password_entry.delete(0, END)
+                website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -46,7 +77,7 @@ password_label.grid(column=0, row=3)
 password_entry = Entry(width=22)
 password_entry.grid(column=1, row=3)
 
-generate_password_btn = Button(text="Generate Password")
+generate_password_btn = Button(text="Generate Password", command=password_generator)
 generate_password_btn.grid(column=2, row=3)
 
 add_btn = Button(text="Add", width=37, command=save)
